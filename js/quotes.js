20a27ec957039dadf340adf68e29d6d55f05ae53
leedo97y <dlehdus1433@gmail.com>
2022-06-29 18:14:24 +0900

    [add] quote part, and quotes

---
+++ js/quotes.js
@@ -0,0 +1,32 @@
+/**
+ * 1. I never dreamed about success, I worked for it.
+ * - Estee Lauder
+ *
+ * 2. Do not try to be original, just try to be good.
+ * - Paul Rand
+ *
+ * 3. Do not be afraid to give up the good to go forthe great.
+ * - John D. Rockefeller
+ *
+ * 4. The fastest way to change yourself is to hang out with people who are already the way you want to be.
+ * - Reid Hoffman
+ *
+ * 5. Some people dream of success, while other people get up every morning and make it happen.
+ * - Wayne Huizenga
+ *
+ * 6. Be slow to fall into friendship. but when you are in, continue firm & constant.
+ * - Socrates
+ *
+ * 7. All things are difficult before they are easy.
+ * - Thomas Fuller
+ *
+ * 8. What do you want a meaning for? Life is a desire, not a meaning
+ * - Charlie Chaplin
+ *
+ * 9. You may delay, but time will not.
+ * - Benjamin Franklin
+ *
+ * 10. What you risk reveals what you value
+ * - Jeanatte Winterson
+ *
+ */
